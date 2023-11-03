1e84e81eb856fc775ab6597ad0c3184d903f9b70
Bebe Jane <bjornthief@gmail.com>
2023-11-03 11:43:31 +0100

    node endpoint

--- app/api/revalidate-tag/route.ts
+++ app/api/revalidate-tag/route.ts
@@ -1,7 +1,7 @@
 
 import revalidateTagHandler from '@lib/dato-nextjs-utils/route-handlers/revalidate-tag';
 
-export const runtime = "edge"
+export const runtime = "nodejs"
 
 export async function POST(req: Request) {
   return await revalidateTagHandler(req)
