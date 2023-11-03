7bf2c6e44129f005ac449e7c7c1e3a46324d54b4
Bebe Jane <bjornthief@gmail.com>
2023-11-04 00:08:11 +0100

    preview

--- app/datocms/preview/route.ts
+++ app/datocms/preview/route.ts
@@ -17,6 +17,13 @@ export async function GET(request: Request) {
   const bypassCookie = cookies().get('__prerender_bypass');
   console.log(bypassCookie)
   console.log(cookies().getAll())
-  //cookies().set('__prerender_bypass', bypassCookie.value, {maxAge:3})
+
+  cookies().set(bypassCookie.name, bypassCookie.value, {
+    httpOnly: true,
+    sameSite: 'none',
+    secure: true,
+    path: '/',
+    maxAge: 3
+  })
   redirect(slug)
 }
