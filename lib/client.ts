0f67b1c3a1df7e3c86004b802423812d6a5bc051
Bebe Jane <bjornthief@gmail.com>
2023-11-03 12:56:36 +0100

    fix

--- lib/client.ts
+++ lib/client.ts
@@ -27,7 +27,7 @@ const defaultApiQueryOptions: ApiQueryOptions = {
 
 export async function apiQuery<T>(query: DocumentNode, options: ApiQueryOptions = defaultApiQueryOptions) {
 
-  options = { ...defaultApiQueryOptions, ...options }
+  options = { ...defaultApiQueryOptions, ...options, revalidate: 0 }
 
   const queryName = (query.definitions?.[0] as any).name?.value as string
   const dedupeOptions: DedupeOptions = {
