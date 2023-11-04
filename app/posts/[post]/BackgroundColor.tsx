fce79b45af4e73692ab1bfa4280af71af112f6ca
Bebe Jane <bjornthief@gmail.com>
2023-11-04 23:24:19 +0100

    back

--- app/posts/[post]/BackgroundColor.tsx
+++ app/posts/[post]/BackgroundColor.tsx
@@ -1,18 +1,19 @@
 'use client'
 
-import { useEffect } from "react"
-import { apiQuery } from "@lib/client"
-import { PostDocument } from "@graphql"
-import { useParams } from "next/navigation"
+import { useEffect, useRef } from "react"
 
 export default function BackgroundColor({ color }) {
 
+  const originalColor = useRef<string | null>(null)
+
   useEffect(() => {
+    if (!originalColor.current)
+      originalColor.current = document.body.style.backgroundColor
 
     document.body.style.backgroundColor = color
 
     return () => {
-      document.body.style.backgroundColor = ''
+      document.body.style.backgroundColor = originalColor.current
     }
   }, [color])
 
