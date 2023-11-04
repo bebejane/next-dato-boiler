c0e59a15d01689176dad0e440d951e03e3a93609
Bebe Jane <bjornthief@gmail.com>
2023-11-04 10:35:10 +0100

    force

--- app/datocms/revalidate-tag/route.ts
+++ app/datocms/revalidate-tag/route.ts
@@ -2,7 +2,7 @@
 import revalidateTagHandler from '@lib/dato-nextjs-utils/route-handlers/revalidate-tag';
 
 export const runtime = "nodejs"
-//export const dynamic = "force-dynamic"
+export const dynamic = "force-dynamic"
 
 export async function POST(req: Request) {
   return await revalidateTagHandler(req)
