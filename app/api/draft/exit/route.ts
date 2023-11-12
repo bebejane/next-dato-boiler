9333eb90d39815cf1a20880dcf81aed4805efe89
Bebe Jane <bjornthief@gmail.com>
2023-11-12 02:39:22 +0100

    finished

--- app/api/draft/exit/route.ts
+++
@@ -1,9 +0,0 @@
-import { disableDraftMode } from './action'
-
-export const runtime = "edge"
-export const dynamic = "force-dynamic"
-
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url)
-  disableDraftMode(searchParams.get('pathname'))
-}
--- app/api/draft/route.ts
+++ app/api/draft/route.ts
@@ -1,31 +1,3 @@
-import { draftMode } from 'next/headers'
-import { redirect } from 'next/navigation'
-import { cookies } from 'next/headers'
-
+export { draft as GET } from 'next-dato-utils'
 export const dynamic = 'force-dynamic'
 export const runtime = "edge"
-
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url)
-  const secret = searchParams.get('secret')
-  const slug = searchParams.get('slug')
-  const maxAge = searchParams.get('max-age')
-
-  if (secret !== process.env.DATOCMS_PREVIEW_SECRET || !slug)
-    return new Response('Invalid token', { status: 401 })
-
-  draftMode().enable()
-
-  if (maxAge) {
-    const bypassCookie = cookies().get('__prerender_bypass');
-    cookies().set(bypassCookie.name, bypassCookie.value, {
-      httpOnly: true,
-      sameSite: 'none',
-      secure: true,
-      path: '/',
-      maxAge: parseInt(maxAge)
-    })
-  }
-
-  redirect(slug)
-}
