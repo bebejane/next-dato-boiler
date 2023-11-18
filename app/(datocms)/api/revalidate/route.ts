343102ce5f57558923643d1b0720339afd232ec2
Bebe Jane <bjornthief@gmail.com>
2023-11-18 21:07:33 +0100

    t

--- app/(datocms)/api/revalidate/route.ts
+++ app/(datocms)/api/revalidate/route.ts
@@ -2,6 +2,7 @@ import { revalidate } from 'next-dato-utils'
 
 export const runtime = "edge"
 export const dynamic = "force-dynamic"
+export const fetchCache = "force-no-store";
 
 export async function POST(req: Request) {
 
