d991c1456dad0d97c229cd740b89c6bde17170f1
Bebe Jane <bjornthief@gmail.com>
2024-05-20 15:12:56 +0200

    fix

--- lib/config.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigDocument } from "@graphql";
-import { apiQuery } from "next-dato-utils/api";
-
-export default async function getConfig(): Promise<ConfigQuery['config']> {
-  return (await apiQuery<ConfigQuery, ConfigQueryVariables>(ConfigDocument, {
-    tags: ['config']
-  })).config;
-}
--- test.ts
+++
@@ -1,7 +0,0 @@
-import { apiQuery } from "next-dato-utils/api";
-import { AllPostsTestDocument } from "@graphql";
-
-(async () => {
-  const data = await apiQuery<AllPostsTestQuery, AllPostsTestQueryVariables>(AllPostsTestDocument);
-  console.log(data)
-})()
