bd8961333f67eb1ea68722cabab0939de645eeb6
Bebe Jane <bjornthief@gmail.com>
2026-02-26 20:58:36 +0100

    p

--- next.config.ts
+++ next.config.ts
@@ -50,6 +50,10 @@ const nextConfig: NextConfig = {
 					{ key: 'Access-Control-Allow-Credentials', value: 'true' },
 					{ key: 'Access-Control-Allow-Origin', value: '*' },
 					{ key: 'Access-Control-Allow-Methods', value: 'POST,OPTIONS' },
+					{ 
+						key: 'Content-Security-Policy',
+    				value: "frame-ancestors 'self' https://plugins-cdn.datocms.com"
+					},
 					{
 						key: 'Access-Control-Allow-Headers',
 						value:
