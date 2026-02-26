a168ba434dd9beef3dc96efcfb2e4bd761712e9c
Bebe Jane <bjornthief@gmail.com>
2026-02-26 21:37:16 +0100

    t

---
+++ app/(datocms)/api/draft-mode/route.ts
@@ -0,0 +1,6 @@
+export const dynamic = 'force-dynamic';
+import config from '@/datocms.config';
+import { draft } from 'next-dato-utils/route-handlers';
+export const GET = async (req: Request, params: any) => {
+	return draft(req, params);
+};
