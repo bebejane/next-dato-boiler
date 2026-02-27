24d2f736426b37e412ef4bb36628ac232a352917
Bebe Jane <bjornthief@gmail.com>
2026-02-27 16:06:37 +0100

    p

--- components/nav/LocaleSwitcher.tsx
+++ components/nav/LocaleSwitcher.tsx
@@ -2,14 +2,18 @@
 
 import s from './LocaleSwitcher.module.scss';
 import { useLocale } from 'next-intl';
-import { Link, locales } from '@/i18n/routing';
+import { Link, locales, usePathname } from '@/i18n/routing';
 
 export function LocaleSwitcher() {
 	const locale = useLocale();
+	const pathname = usePathname();
 
 	return (
 		<nav className={s.locales} key={locale}>
 			<ul>
+				<li>
+					<a href={`/api/draft?secret=99E3GxyXr9pGy1QD&slug=${pathname}`}>Draft</a>
+				</li>
 				{locales.map((l, i) => (
 					<li key={i} className={locale === l ? s.selected : undefined}>
 						<Link href={'/'} locale={l}>
