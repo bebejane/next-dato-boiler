b4cd0fde626711ba4946a740186f628753ad9872
Bebe Jane <bjornthief@gmail.com>
2026-02-27 01:07:55 +0100

    h

--- app/[locale]/posts/[post]/loading.tsx
+++ app/[locale]/posts/[post]/loading.tsx
@@ -3,7 +3,7 @@ import s from './loading.module.scss';
 export default async function Loading() {
 	return (
 		<div className={s.loading}>
-			<span>Loading...</span>
+			<span></span>
 		</div>
 	);
 }
