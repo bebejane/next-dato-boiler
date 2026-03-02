fa700862b240e46a8ee9b6543d1700d1322b5011
Bebe Jane <bjornthief@gmail.com>
2026-03-02 10:41:30 +0100

    utils dev update

--- next.config.ts
+++ next.config.ts
@@ -64,19 +64,6 @@ const nextConfig: NextConfig = {
 				],
 			},
 			{
-				source: '/api/draft-mode',
-				headers: [
-					{ key: 'Access-Control-Allow-Credentials', value: 'true' },
-					{ key: 'Access-Control-Allow-Origin', value: '*' },
-					{ key: 'Access-Control-Allow-Methods', value: 'POST,OPTIONS' },
-					{
-						key: 'Access-Control-Allow-Headers',
-						value:
-							'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-					},
-				],
-			},
-			{
 				source: '/api/backup',
 				headers: [
 					{ key: 'Access-Control-Allow-Credentials', value: 'true' },
