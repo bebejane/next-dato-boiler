16b4908f31b7b036f401cd541209a7ea17a07cb2
Bebe Jane <bjornthief@gmail.com>
2026-03-20 22:41:59 +0100

    utils dev update

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,4 +1,5 @@
 import { DOMAttributes } from 'react';
+import 'react';
 
 declare module 'react' {
 	interface HTMLAttributes<T> extends DOMAttributes<T> {
@@ -6,6 +7,22 @@ declare module 'react' {
 		'data-datocms-content-link-boundary'?: boolean;
 		'data-datocms-content-link-source'?: string | null;
 		'data-datocms-content-link-url'?: string | null;
-		'data-datocms-content-contins-stega'?: string | null;
 	}
+}
+
+declare module 'react' {
+	interface CSSProperties {
+		[key: `--${string}`]: string | number | undefined;
+	}
+}
+
+declare module 'react-balance-text' {
+	const BalanceText: React.ComponentClass<{
+		children?: React.ReactNode;
+		className?: string;
+		style?: React.CSSProperties;
+		resize?: boolean;
+	}>;
+
+	export = BalanceText;
 }
